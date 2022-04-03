7f6e2552d8d493d498da08a1cc9b01071e46bfe6
Felipe <feslima93@gmail.com>
2022-04-03 18:55:18 -0300

    autocompletion and snippets configuration

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,7 +46,19 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used in lots of plugins
   
+  -- Colorscheme
   use "LunarVim/darkplus.nvim" -- Very similar with VSCode colors (you need TreeSiter to have good syntax highlighting
+
+  -- Code completion (cmp)
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- Snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
