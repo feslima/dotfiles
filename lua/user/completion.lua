86cfd2bbe00bbf2ae8e3a64a8d7d9242b006911d
Felipe <feslima93@gmail.com>
2022-04-09 15:33:09 -0300

    completion for NVim lua files

--- lua/user/completion.lua
+++ lua/user/completion.lua
@@ -105,6 +105,8 @@ cmp.setup {
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[NVim Lua]",
         luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
@@ -114,6 +116,7 @@ cmp.setup {
   },
   sources = {
     { name = "nvim_lsp" },
+    { name = "nvim_lua" },
     { name = "luasnip" },
     { name = "buffer" },
     { name = "path" },
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,6 +56,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp" -- additional sources of completion for the LSP client
+  use "hrsh7th/cmp-nvim-lua" -- completion for nvim lua config files
 
   -- Snippets
   use "L3MON4D3/LuaSnip" --snippet engine
