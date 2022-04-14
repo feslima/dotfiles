a2be0f4af77dc6f4c0c289e02925a21c45a33710
Felipe <feslima93@gmail.com>
2022-04-14 08:55:13 -0300

    project.nvim integration

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -106,6 +106,9 @@ return packer.startup(function(use)
   -- Toggleterm
   use "akinsho/toggleterm.nvim" -- allows terminal use when editing in nvim
 
+  -- Project
+  use "ahmedkhalf/project.nvim" -- allows project-like management
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
