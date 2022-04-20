f7d739b174451a1a85afda37ccd6b1b53121dabb
Felipe <feslima93@gmail.com>
2022-04-20 19:01:59 -0300

    enabling filename on lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,84 +1,84 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-  vim.notify("Couldn't load 'lualine' plugin!")
-  return
+	vim.notify("Couldn't load 'lualine' plugin!")
+	return
 end
 
 local branch = {
-  "branch",
-  icons_enabled = true,
-  icon = "",
+	"branch",
+	icons_enabled = true,
+	icon = "",
 }
 
-
 local diagnostics = {
-  "diagnostics",
-  sources = { "nvim_diagnostic" },
-  sections = { "error", "warn" },
-  symbols = { error = " ", warn = " " },
-  colored = false,
-  update_in_insert = false,
-  always_visible = true,
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
+	colored = false,
+	update_in_insert = false,
+	always_visible = true,
 }
 
 local diff = {
-  "diff",
-  colored = false,
-  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = function() return vim.fn.winwidth(0) > 80 end
+	"diff",
+	colored = false,
+	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+	cond = function()
+		return vim.fn.winwidth(0) > 80
+	end,
 }
 
 local spaces = function()
-  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
 local filetype = {
-  "filetype",
-  icons_enabled = false,
-  icon = nil,
+	"filetype",
+	icons_enabled = false,
+	icon = nil,
 }
 
-
 local location = {
-  "location",
-  padding = 0,
+	"location",
+	padding = 0,
 }
 
 local progress = function()
-  local current_line = vim.fn.line(".")
-  local total_lines = vim.fn.line("$")
-  local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-  local line_ratio = current_line / total_lines
-  local index = math.ceil(line_ratio * #chars)
-  return chars[index]
+	local current_line = vim.fn.line(".")
+	local total_lines = vim.fn.line("$")
+	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	local line_ratio = current_line / total_lines
+	local index = math.ceil(line_ratio * #chars)
+	return chars[index]
 end
 
 lualine.setup({
-  options = {
-    icons_enabled = true,
-    theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    -- disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", "dapui_stacks", "dapui_watches" },
-    disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = { branch, diagnostics },
-    lualine_b = { "mode" },
-    lualine_c = {},
-    lualine_x = { diff, spaces, "encoding", filetype },
-    lualine_y = { location },
-    lualine_z = { progress },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  extensions = {},
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		-- disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", "dapui_stacks", "dapui_watches" },
+		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { branch, diagnostics },
+		lualine_b = { "mode" },
+		lualine_c = { "filename" },
+		lualine_x = { diff, spaces, "encoding", filetype },
+		lualine_y = { location },
+		lualine_z = { progress },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
 })
