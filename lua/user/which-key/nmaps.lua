1c550a6910bfd957c6f82af4e0e3f56c1f47735a
Felipe <feslima93@gmail.com>
2022-04-25 14:33:01 -0300

    fix missing <CR>

--- lua/user/which-key/nmaps.lua
+++ lua/user/which-key/nmaps.lua
@@ -162,7 +162,7 @@ local M = {
 		},
 		u = {
 			name = "Terminal utils",
-			a = { "<cmd>ToggleTermToggleAll", "Toggle all terminals" },
+			a = { "<cmd>ToggleTermToggleAll<CR>", "Toggle all terminals" },
 			c = { "<cmd>lua _CYPRESS_TOGGLE()<CR>", "Open/toggle cypress panel" },
 			d = { "<cmd>lua _LAZYDOCKER_TOGGLE()<CR>", "Toggle LazyDocker Terminal" },
 			f = { "<cmd>Telescope termfinder<CR>", "Find terminals" },
