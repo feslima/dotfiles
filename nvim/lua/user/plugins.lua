a83d0adedff9ab741f5f83d1803d3ef8df2e9512
Felipe <feslima93@gmail.com>
2022-05-02 20:25:36 -0300

    nvim-lightbulb integration

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -169,6 +169,9 @@ return packer.startup(function(use)
 	-- Code outlines
 	use("simrat39/symbols-outline.nvim")
 
+	-- Lightbulb
+	use("kosayoda/nvim-lightbulb") -- code actions like VSCode
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
