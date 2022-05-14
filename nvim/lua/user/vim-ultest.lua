ae1107f9bad699bcd70b945e2e2ee464c967a8e6
Felipe <26683783+feslima@users.noreply.github.com>
2022-05-14 14:48:32 -0300

    create .env file utils module

--- nvim/lua/user/vim-ultest.lua
+++ nvim/lua/user/vim-ultest.lua
@@ -5,48 +5,10 @@ g.ultest_fail_sign = ""
 g.ultest_running_sign = ""
 g.ultest_not_run_sign = ""
 
-local p_status_ok, Path = pcall(require, "plenary.path")
-if not p_status_ok then
-	vim.notify("Couldn't load 'plenary.path'!", "error")
-	return
-end
-
-local _get_variable_name = function(line)
-	return line:match("^([A-Z0-9_]+)=.*$")
-end
-
-local _get_variable_value = function(line)
-	return line:match("^[A-Z0-9_]+=(.*)$")
-end
-
-local _extract_variables_from_file = function(fpath)
-	local collected_envs = {}
-	local file_contents = vim.fn.readfile(fpath)
-	for _, line in ipairs(file_contents) do
-		collected_envs[_get_variable_name(line)] = _get_variable_value(line)
-	end
-	return collected_envs
-end
-
 function _G.set_test_environment_from_file(fpath)
-	local p = Path:new(fpath)
-	local abs = p:absolute()
-	if not p:exists() then
-		vim.notify("Path " .. abs .. " does not exist.", "error")
-		return
-	end
+	local abs = _G.check_env_file_is_valid(fpath)
 
-	if not p:is_file() then
-		vim.notify("Path " .. abs .. " is not a file.", "error")
-		return
-	end
-
-	if abs:match("^.+(%..+)$") ~= ".env" then
-		vim.notify("Path " .. abs .. " is not a .env file.", "error")
-		return
-	end
-
-	local collected_envs = _extract_variables_from_file(abs)
+	local collected_envs = _G._extract_variables_from_file(abs)
 	g.ultest_env = collected_envs
 	vim.notify(vim.inspect(collected_envs), "info", { title = "Successfuly loaded env vars from " .. abs })
 end
