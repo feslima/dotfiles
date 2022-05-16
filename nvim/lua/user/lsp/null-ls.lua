79d1604a5d1a3df6047a389e63c21db48e3389a2
Felipe <26683783+feslima@users.noreply.github.com>
2022-05-16 17:26:58 -0300

    null-ls isort config update

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -4,6 +4,8 @@ if not null_ls_status_ok then
 	return
 end
 
+local lsp_formatting_group_id = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+
 -- List of supported formatters/diagnostics can be found in the links below
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
@@ -19,8 +21,8 @@ null_ls.setup({
 		-- diagnostics.eslint,
 
 		-- Python
-		formatting.isort, -- [pyenv/venv] shell: python -m -pip install isort
 		formatting.black, -- [pyenv/venv] shell: python -m -pip install black
+		formatting.isort.with({ extra_args = { "--profile", "black" } }), -- [pyenv/venv] shell: python -m -pip install isort
 		diagnostics.flake8, -- [pyenv/venv] shell: python -m -pip install flake8
 		-- diagnostics.mypy, -- [pyenv/venv] shell: python -m -pip install mypy
 
@@ -34,13 +36,13 @@ null_ls.setup({
 		-- TOML
 		formatting.taplo, -- [rust] shell: cargo install taplo-cli
 	},
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			local lsp_formatting_group_id = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = lsp_formatting_group_id, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = lsp_formatting_group_id,
 				pattern = "<buffer>",
-				command = "lua vim.lsp.buf.formatting_sync()",
+				callback = vim.lsp.buf.formatting_sync,
 				desc = "Format on save if client has formatting capabilities",
 			})
 		end
