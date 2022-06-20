b0d573f686a584265fe130c5f4e3b2126fc018b4
Felipe <26683783+feslima@users.noreply.github.com>
2022-06-20 14:52:37 -0300

    disable html LSP formatting (null-ls only)

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -65,7 +65,7 @@ end
 local function lsp_keymaps(bufnr) end
 
 M.on_attach = function(client, bufnr)
-	local disabled_formatting = { "tsserver", "sumneko_lua", "taplo", "jsonls", "gopls" }
+	local disabled_formatting = { "tsserver", "sumneko_lua", "taplo", "jsonls", "gopls", "html" }
 	if vim.tbl_contains(disabled_formatting, client.name) then
 		client.resolved_capabilities.document_formatting = false
 	end
