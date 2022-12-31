07ee934948e9f585cf3ce939ed3eaf4f4d5ec99e
Felipe <26683783+feslima@users.noreply.github.com>
2022-12-31 11:53:34 -0300

    vim-visual-multi

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -210,6 +210,9 @@ return packer.startup(function(use)
 	-- Peek definitions/references
 	use({ "rmagatti/goto-preview", requires = "nvim-telescope/telescope.nvim" })
 
+	-- Mutliple cursor
+	use({ "mg979/vim-visual-multi" })
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
