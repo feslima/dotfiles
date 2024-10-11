613a666dd532bcb70bdc7dfb10249bfe2b1467d6
Felipe <26683783+feslima@users.noreply.github.com>
2024-10-11 18:01:23 -0300

    feat: remote-nvim & telescope-cmdline

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -128,8 +128,15 @@ local plugins = {
 	},
 	{
 		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"jonarrien/telescope-cmdline.nvim",
+		},
 		cmd = "Telescope",
+		keys = {
+			{ ":", "<cmd>Telescope cmdline<cr>", desc = "Cmdline" },
+		},
 		opts = function()
 			return require("plugins.telescope")
 		end,
@@ -482,6 +489,16 @@ local plugins = {
 		config = function(_, opts)
 			require("scrollview").setup(opts)
 		end,
+	},
+	{
+		"amitds1997/remote-nvim.nvim",
+		version = "*", -- Pin to GitHub releases
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- For standard functions
+			"MunifTanjim/nui.nvim", -- To build the plugin UI
+			"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+		},
+		config = true,
 	},
 }
 
